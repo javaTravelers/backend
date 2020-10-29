eb532c0075a0d2783c3de276cd23b9d2421e14b5
Adriano Almeida <adriano.estudio8@gmail.com>
2020-10-29 11:05:43 -0300

    Spring Boot Project Creation

---
+++ src/main/java/br/com/javatravelers/JavaTravelers/JavaTravelersApplication.java
@@ -0,0 +1,13 @@
+package br.com.javatravelers.JavaTravelers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTravelersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTravelersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/javatravelers/JavaTravelers/JavaTravelersApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.javatravelers.JavaTravelers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTravelersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
