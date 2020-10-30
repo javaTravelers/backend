507bce072527e07dde2792df94c7fb4325a6022c
Luiz Eduardo Bilotta <luizeduardobilotta@gmail.com>
2020-10-30 08:47:57 -0300

    Created interface UserRepository.

--- src/main/java/br/com/javatravelers/JavaTravelers/domain/repository/UserRepository.java
+++ src/main/java/br/com/javatravelers/JavaTravelers/domain/repository/UserRepository.java
@@ -1,5 +1,5 @@
 package br.com.javatravelers.JavaTravelers.domain.repository;
-
+//
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
