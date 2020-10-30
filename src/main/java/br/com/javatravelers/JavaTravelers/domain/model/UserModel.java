6c68dd5f9ad03b5e6ac70459596a2eccc19448ff
Luiz Eduardo Bilotta <luizeduardobilotta@gmail.com>
2020-10-30 08:33:53 -0300

    Created class UserModel.

---
+++ src/main/java/br/com/javatravelers/JavaTravelers/domain/model/UserModel.java
@@ -0,0 +1,56 @@
+package br.com.javatravelers.JavaTravelers.domain.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class UserModel implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	
+	@NotBlank(message="O campo nome deve ser preenchido.")
+	@Size(max = 80, message = "O nome deve conter no máximo 50 caracteres.")
+	@Column(length = 80, nullable = false)
+	private String nome;
+	
+	@Email(message = "O endereço de e-mail é inválido!")
+	@NotBlank(message = "O campo e-mail deve ser preenchido.")
+	@Size(max = 60, message = "O endereço de e-mail é muito grande.")
+	@Column(length = 60, nullable = false)
+	private String email;
+	
+	@NotBlank(message = "O campo senha deve ser preenchido.")
+	@Size(max = 20, message = "A senha deve conter no máximo 20 caracteres.")
+	@Column(length = 20, nullable = false)
+	private String senha;
+	
+	@NotBlank(message = "O campo cpf deve ser preenchido.")
+	@Size(max = 11, message = "Preencha com um cpf válido!")
+	@Column(length = 11, nullable = false)
+	private String cpf;
+	
+}
