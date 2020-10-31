f2caec63f5f4dcf289074db77cfe0f55f5f05600
Luiz Eduardo Bilotta <luizeduardobilotta@gmail.com>
2020-10-31 18:18:27 -0300

    Created userController class

---
+++ src/main/java/br/com/javatravelers/JavaTravelers/api/controller/UserController.java
@@ -0,0 +1,50 @@
+package br.com.javatravelers.JavaTravelers.api.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.javatravelers.JavaTravelers.domain.model.UserModel;
+import br.com.javatravelers.JavaTravelers.domain.repository.UserRepository;
+import br.com.javatravelers.JavaTravelers.domain.service.UserService;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<UserModel> saveUser(@Valid @RequestBody UserModel userModel) {
+		return new ResponseEntity<UserModel>(userService.saveUser(userModel), HttpStatus.CREATED);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteUser(@PathVariable Integer id) {
+		
+		if(!userRepository.existsById(id)) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	
+		userRepository.deleteById(id);
+		return new ResponseEntity<>(HttpStatus.OK);
+
+	}
+	
+	
+}
+
