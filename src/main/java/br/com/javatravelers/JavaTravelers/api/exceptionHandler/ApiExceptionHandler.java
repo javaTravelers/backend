76ca394470f3797fac6992669be7a31c37ae422f
Adriano Almeida <adriano.estudio8@gmail.com>
2020-11-03 21:17:37 -0300

    Created TicketException and Updated AmadeusService

--- src/main/java/br/com/javatravelers/JavaTravelers/api/exceptionHandler/ApiExceptionHandler.java
+++ src/main/java/br/com/javatravelers/JavaTravelers/api/exceptionHandler/ApiExceptionHandler.java
@@ -18,6 +18,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import br.com.javatravelers.JavaTravelers.domain.exception.BusinnesException;
+import br.com.javatravelers.JavaTravelers.service.amadeus.exception.TicketException;
 import lombok.var;
 
 @ControllerAdvice
@@ -30,6 +31,19 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
 	public ResponseEntity<Object> handleDuplicatedEmail(BusinnesException ex, WebRequest request) {
 		var status = HttpStatus.UNPROCESSABLE_ENTITY;
 		
+		var problem = new Problem();
+		problem.setStatus(status.value());
+		problem.setTitulo(ex.getMessage());
+		problem.setDataHora(LocalDateTime.now());
+		
+		return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
+	}
+	
+	@ExceptionHandler(TicketException.class)
+	public ResponseEntity<Object> handleTicketErrors(TicketException ex, WebRequest request) {
+		var status = HttpStatus.UNPROCESSABLE_ENTITY;
+		var statusCode = ex.getStatusCode();
+		System.out.println(statusCode);
 		var problem = new Problem();
 		problem.setStatus(status.value());
 		problem.setTitulo(ex.getMessage());
--- src/main/java/br/com/javatravelers/JavaTravelers/service/amadeus/AmadeusService.java
+++ src/main/java/br/com/javatravelers/JavaTravelers/service/amadeus/AmadeusService.java
@@ -3,6 +3,7 @@ package br.com.javatravelers.JavaTravelers.service.amadeus;
 
 import java.util.Map;
 
+import org.hibernate.internal.build.AllowSysOut;
 import org.springframework.stereotype.Service;
 
 import com.amadeus.Amadeus;
@@ -10,10 +11,16 @@ import com.amadeus.Params;
 import com.amadeus.exceptions.ResponseException;
 import com.amadeus.referenceData.Locations;
 import com.amadeus.resources.Location;
+import com.amadeus.resources.Traveler;
+import com.amadeus.resources.Traveler.Document;
+import com.amadeus.resources.Traveler.Phone;
 import com.amadeus.resources.FlightOfferSearch;
+import com.amadeus.resources.FlightOrder;
 import com.amadeus.resources.FlightPrice;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
 import br.com.javatravelers.JavaTravelers.domain.exception.BusinnesException;
 import br.com.javatravelers.JavaTravelers.service.amadeus.resource.SearchLocation;
@@ -42,14 +49,14 @@ public class AmadeusService {
 					.and("adults", request.get("adults"))
 					.and("currencyCode", "BRL")
 					.and("max", "1"));
-
-			System.out.println("Ofertas de Vôos");
-			System.out.println(flightOffers[0]);
+			
+			if (flightOffers.length <= 0) {
+				throw new BusinnesException("[400] - Não foram encontrados vôos com esses parâmetros.");
+			}
 
 			jsons = gson.toJson(flightOffers);
 		} catch (ResponseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			throw new BusinnesException(e.getMessage().replaceAll("\n", " - "));
 		}
 		return jsons;
 	}
@@ -63,12 +70,11 @@ public class AmadeusService {
 			flightOffers = amadeus.shopping.flightOffersSearch.post(flight);
 			jsons = gson.toJson(flightOffers, FlightOfferSearch[].class);
 		} catch (ResponseException e) {
-			e.printStackTrace();
+			throw new BusinnesException(e.getMessage().replaceAll("\n", " - "));
 		}
 		return jsons;
 	}
 
-	
 	public String searchLocations(SearchLocation search) {
 		// Airport & City Search (autocomplete)
 		// Find all the cities and airports starting by the keyword 'LON'
@@ -80,12 +86,11 @@ public class AmadeusService {
 					.with("keyword", search.getLocation())
 					.and("subType", Locations.ANY));
 			response = json.toJson(locations);
-			if(locations[0].getResponse().getStatusCode() != 200) {
-				System.out.println("Wrong status code: " + locations[0].getResponse().getStatusCode());
-				System.exit(-1);
+			if(locations.length <= 0) {
+				throw new BusinnesException("[400] Local não encontrado.");
 			}
 		} catch (ResponseException e) {
-			throw new BusinnesException(e.getMessage());
+			throw new BusinnesException(e.getMessage().replaceAll("\n", " - "));
 		}
 		return response;
 	}
@@ -98,11 +103,66 @@ public class AmadeusService {
 			FlightPrice flightPricing;
 			flightPricing = amadeus.shopping.flightOffersSearch.pricing.post(
 					flight);
-			response = json.toJson(flightPricing);
-			System.out.println(flightPricing);
+			if (flightPricing.getResponse().getStatusCode() != 200) {
+				throw new BusinnesException("Erro ao localizar a passagem!");
+			}
+			response = json.toJson(flightPricing.getResponse().getResult());
 		} catch (ResponseException e) {
-			throw new BusinnesException(e.getMessage());
+			throw new BusinnesException(e.getMessage().replaceAll("\n", " - "));
 		}
+		return response;
+	}
+	
+	public String createOrder(String flight) {
+		Gson json = new Gson();
+		String response = null;
+		
+		Traveler traveler = new Traveler();
+
+	    traveler.setId("1");
+	    traveler.setDateOfBirth("2000-04-14");
+	    traveler.setName(traveler.new Name("JORGE", "GONZALES"));
+
+	    Traveler.Phone[] phone = new Phone[1];
+	    phone[0] = traveler.new Phone();
+	    phone[0].setCountryCallingCode("33");
+	    phone[0].setNumber("675426222");
+	    phone[0].setDeviceType("MOBILE");
+
+	    Traveler.Contact contact = traveler.new Contact();
+	    contact.setPhones(phone);
+	    traveler.setContact(contact);
+
+	    Traveler.Document[] document = new Document[1];
+	    document[0] = traveler.new Document();
+	    document[0].setDocumentType("PASSPORT");
+	    document[0].setNumber("480080076");
+	    document[0].setExpiryDate("2022-10-11");
+	    document[0].setIssuanceCountry("ES");
+	    document[0].setNationality("ES");
+	    document[0].setHolder(true);
+	    traveler.setDocuments(document);
+
+	    Traveler[] travelerArray = new Traveler[1];
+	    travelerArray[0] = traveler;
+
+
+	    // We book the flight previously priced
+	    FlightOrder order = null;
+		try {
+			order = amadeus.booking.flightOrders.post(flight);
+			System.out.println(order);
+			if (order == null)
+				throw new BusinnesException("A passagem solicitada não existe.");
+			
+			if (order.getResponse().getStatusCode() != 200 && order.getResponse().getStatusCode() != 201)
+				throw new BusinnesException("" + order.getResponse().getStatusCode());
+
+		} catch (ResponseException e) {
+			throw new BusinnesException(e.getMessage().replaceAll("\n", " - "));
+		}
+	    response = json.toJson(order.getResponse().getResult());
+		
 		return response;
 	}
 }
